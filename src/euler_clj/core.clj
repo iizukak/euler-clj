9bd4363dca200c9b7a13483f4f4e1eccabb2411b
Kentaro Iizuka <iizuka.kentaro@gmail.com>
2014-12-31 01:58:16 +0900

    solve problem 33

--- src/euler_clj/core.clj
+++ src/euler_clj/core.clj
@@ -1,15 +1,27 @@
 (ns euler-clj.core
   (:require [clojure.math.combinatorics :as combo]))
 
-(def p (combo/permutations (vec (range 1 10 1))))
+(defn problem-33 []
+    (let [p (combo/permutations (vec (range 1 10)))
 
-; シーケンス a がパンデジタル数であるか？
-(defn is_pandigital [s]
-  (let [m  (+                                      (* 10 (nth s 0)) (nth s 1))
-        n  (+                    (* 100 (nth s 2)) (* 10 (nth s 3)) (nth s 4))
-        mn (+ (* 1000 (nth s 5)) (* 100 (nth s 6)) (* 10 (nth s 7)) (nth s 8))]
-    (println m n mn)
-    (= (* m n) mn)
-    ))
+          is-pandigital-2-3-4 (fn [s]
+          (let [m  (+                                      (* 10 (nth s 0)) (nth s 1))
+                n  (+                    (* 100 (nth s 2)) (* 10 (nth s 3)) (nth s 4))
+                mn (+ (* 1000 (nth s 5)) (* 100 (nth s 6)) (* 10 (nth s 7)) (nth s 8))]
+            (if (= (* m n) mn) mn false)))
+
+          is-pandigital-1-4-4 (fn [s]
+          (let [k  (nth s 0)
+                l  (+ (* 1000 (nth s 1)) (* 100 (nth s 2)) (* 10 (nth s 3)) (nth s 4))
+                kl (+ (* 1000 (nth s 5)) (* 100 (nth s 6)) (* 10 (nth s 7)) (nth s 8))]
+            (if (= (* k l) kl) kl false)))]
+
+      (loop [p p ans ()]
+        (if (seq p)
+          (let [n (or (is-pandigital-2-3-4 (first p)) (is-pandigital-1-4-4 (first p)))]
+            (if n
+              (recur (rest p) (cons n ans))
+              (recur (rest p) ans)))
+          (apply + (distinct ans))))))
 
 
