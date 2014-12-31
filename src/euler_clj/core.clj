e71fe98945c0d40689224ba07d0f58999a5e07dd
Kentaro Iizuka <iizuka.kentaro@gmail.com>
2014-12-31 11:23:41 +0900

    refactor

--- src/euler_clj/core.clj
+++ src/euler_clj/core.clj
@@ -2,26 +2,28 @@
   (:require [clojure.math.combinatorics :as combo]))
 
 (defn problem-33 []
-    (let [p (combo/permutations (vec (range 1 10)))
+  (let [p (combo/permutations (vec (range 1 10)))
 
-          is-pandigital-2-3-4 (fn [s]
+        ; ex: 12 x 483 = 5796
+        is-pandigital-2-3-4 (fn [s]
           (let [m  (+                                      (* 10 (nth s 0)) (nth s 1))
                 n  (+                    (* 100 (nth s 2)) (* 10 (nth s 3)) (nth s 4))
                 mn (+ (* 1000 (nth s 5)) (* 100 (nth s 6)) (* 10 (nth s 7)) (nth s 8))]
             (if (= (* m n) mn) mn false)))
 
-          is-pandigital-1-4-4 (fn [s]
+        ; ex: 12 x 483 = 5796
+        is-pandigital-1-4-4 (fn [s]
           (let [k  (nth s 0)
                 l  (+ (* 1000 (nth s 1)) (* 100 (nth s 2)) (* 10 (nth s 3)) (nth s 4))
                 kl (+ (* 1000 (nth s 5)) (* 100 (nth s 6)) (* 10 (nth s 7)) (nth s 8))]
             (if (= (* k l) kl) kl false)))]
 
-      (loop [p p ans ()]
-        (if (seq p)
-          (let [n (or (is-pandigital-2-3-4 (first p)) (is-pandigital-1-4-4 (first p)))]
-            (if n
-              (recur (rest p) (cons n ans))
-              (recur (rest p) ans)))
-          (apply + (distinct ans))))))
+    (loop [p p ans ()]
+      (if (seq p)
+        (let [n (or (is-pandigital-2-3-4 (first p)) (is-pandigital-1-4-4 (first p)))]
+          (if n
+            (recur (rest p) (cons n ans))
+            (recur (rest p) ans)))
+        (apply + (distinct ans)))))) ; answer: 45228
 
 
